a6ffce0ad9e176a40bfa2b0bf9e194186bff1609
annmunyare <annmunyare28@yahoo.com>
2017-03-23 09:19:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nyambura/myrestaurant/ui/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.nyambura.myrestaurant.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.graphics.Typeface;
+
+import com.example.nyambura.myrestaurant.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Typeface robotoItalicFont = Typeface.createFromAsset(getAssets(), "fonts/robotoItalic.ttf");
+        mAppNameTextView.setTypeface(robotoItalicFont);
+        mFindRestaurantsButton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
+}
